fcf657c5849d2d8099ca9ef9aab787366db8988b
Antti Juustila <antti.juustila@gmail.com>
2022-06-17 12:15:41 +0300

    Finished C version of x-or-fun

--- fun/x-or-fun.c
+++ fun/x-or-fun.c
@@ -2,50 +2,62 @@
 #include <stdio.h>
 #include <limits.h>
 
+int max(int array[], int size);
+void printArray(int array[], int size);
 void printBits(size_t const size, void const * const ptr);
 
 int main(int argc, char * argv[]) {
-
-   int [] array = {1, 2, 3, 4, 6, 7, 8, 9, 10};
-   printArray(array);
-
-   int maxValue = max(array);
+   
+   int array[] = {1, 2, 3, 4, 6, 7, 8, 9, 10};
+   int arraySize = sizeof(array)/sizeof(int);
+   printArray(array, arraySize);
+   
+   int maxValue = max(array, arraySize);
    int result = 0;
-
+   printf("First loop...\n");
+   for (int number = 1; number <= maxValue; number++) {
+      result = result ^ number;
+   }
+   printf("Second loop...\n");
+   for (int index = 0; index < arraySize; index++) {
+      result = result ^ array[index];
+   }
+   printf("Missing number: %d\n", result);
    return EXIT_SUCCESS;
 }
 
-int max(int [] array) {
-   int max = INT_MIN;
-   for (int i = 0; i < sizeof(array)/sizeof(int); i++) {
-      if (array[i] > max {
-         max = array[i];
+int max(int array[], int size) {
+   int maxValue = INT_MIN;
+   for (int i = 0; i < size; i++) {
+      if (array[i] > maxValue) {
+         maxValue = array[i];
       }
-          }
-          return max;
-          }
+   }
+   return maxValue;
+}
 
-          void printArray(int [] array) {
-         for (int i = 0; i < sizeof(array)/sizeof(int); i++) {
-            printf("%d, ", array[i]);
+void printArray(int array[], int size) {
+   printf("Array contents: \n");
+   for (int i = 0; i < size; i++) {
+      printf("%d, ", array[i]);
+   }
+   printf("\n");
+}
+
+void printBits(size_t const size, void const * const ptr)
+{
+   unsigned char *b = (unsigned char*) ptr;
+   unsigned char byte;
+   int i, j;
+   
+   for (i = size-1; i >= 0; i--) {
+      for (j = 7; j >= 0; j--) {
+         byte = (b[i] >> j) & 1;
+         printf("%u", byte);
+         if (j % 4 == 0) {
+            printf(" ");
          }
-         printf("\n");
       }
-
-          void printBits(size_t const size, void const * const ptr)
-          {
-         unsigned char *b = (unsigned char*) ptr;
-         unsigned char byte;
-         int i, j;
-
-         for (i = size-1; i >= 0; i--) {
-            for (j = 7; j >= 0; j--) {
-               byte = (b[i] >> j) & 1;
-               printf("%u", byte);
-               if (j % 4 == 0) {
-                  printf(" ");
-               }
-            }
-         }
-         puts("");
-      }
+   }
+   puts("");
+}
