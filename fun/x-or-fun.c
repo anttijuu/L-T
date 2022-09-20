395ac432b3184bcbf4b5baca064d504768e41cfa
Antti Juustila <antti.juustila@gmail.com>
2022-09-20 19:14:20 +0300

    More output in x-or-fun sample

--- fun/x-or-fun.c
+++ fun/x-or-fun.c
@@ -3,36 +3,44 @@
 #include <limits.h>
 #include <stdbool.h>
 
+// Build: gcc -o xfun x-or-fun.c
+// Run: execute xfun
 
 int max(int array[], int size);
 void printArray(int array[], int size);
 void printBits(size_t const size, void const * const ptr);
 
-bool showDetails = false; // Change to true to see details.
+bool showDetails = true; // Change to true to see details in output, false to hide them.
 
 int main(int argc, char * argv[]) {
    
    int array[] = {1, 2, 3, 4, 6, 7, 8, 9, 10};
    int arraySize = sizeof(array)/sizeof(int);
+	printf("\nThis tool finds a missing number from the array below,\n");
+	printf("using XOR operator. The tool goes through two loops, first\n");
+	printf("XORing the numbers from 1 to max value found in array,\n");
+	printf("(10 in this example). Then second loop XORs the actual values\n");
+	printf("in the array. The result will then contain the missing value.\n\n");
+
    printArray(array, arraySize);
    
    int maxValue = max(array, arraySize);
    int result = 0;
    printf("First loop...\n");
    for (int number = 1; number <= maxValue; number++) {
-      if (showDetails) { printf("  "); printBits(sizeof(int), &result); }
-      if (showDetails) { printf("^ "); printBits(sizeof(int), &number); }
+      if (showDetails) { printf("    "); printBits(sizeof(int), &result); }
+      if (showDetails) { printf("XOR "); printBits(sizeof(int), &number); }
       result = result ^ number;
-      if (showDetails) { printf("> "); printBits(sizeof(int), &result); printf("----\n"); }
+      if (showDetails) { printf("=   "); printBits(sizeof(int), &result); printf("----\n"); }
    }
    printf("Second loop...\n");
    for (int index = 0; index < arraySize; index++) {
-      if (showDetails) { printf("  "); printBits(sizeof(int), &result); }
-      if (showDetails) { printf("^ "); printBits(sizeof(int), &array[index]); }
+      if (showDetails) { printf("    "); printBits(sizeof(int), &result); }
+      if (showDetails) { printf("XOR "); printBits(sizeof(int), &array[index]); }
       result = result ^ array[index];
-      if (showDetails) { printf("> "); printBits(sizeof(int), &result); printf("----\n"); }
+      if (showDetails) { printf("=   "); printBits(sizeof(int), &result); printf("----\n"); }
    }
-   printf("Missing number: %d\nAnd in binary: \n", result);
+   printf("Missing number is: %d\nAnd missing number in binary is: \n", result);
    printBits(sizeof(int), &result);
    return EXIT_SUCCESS;
 }
--- fun/x-or-fun.swift
+++ fun/x-or-fun.swift
@@ -1,29 +1,38 @@
+// Swift version of the x-or-fun.c.
+// Build: swiftc -o xfun x-or-fun.swift
+// Run: execute the xfun binary.
+
 let array = [1,2,3,4,6,7,8,9,10]
-// let array = [1,2,4]
 
 let showDetails = true // Change to true to see what happens
 var result = 0
 let maxNum = array.max()!
 
+print("\nThis tool finds a missing number from the array below,");
+print("using XOR operator. The tool goes through two loops, first");
+print("XORing the numbers from 1 to max value found in array,");
+print("10 in this example). Then second loop XORs the actual values");
+print("in the array. The result will then contain the missing value.\n");
+
 print("Array is \(array)")
 print("First loop...")
 for number in 1...maxNum {
-   if showDetails { print("  \(pad(string: String(result, radix: 2), toSize: 8))") }
-   if showDetails { print("^ \(pad(string: String(number, radix: 2), toSize: 8))") }
+   if showDetails { print("    \(pad(string: String(result, radix: 2), toSize: 8))") }
+   if showDetails { print("XOR \(pad(string: String(number, radix: 2), toSize: 8))") }
    result = result ^ number
-   if showDetails { print("> \(pad(string: String(result, radix: 2), toSize: 8))") }
-   if showDetails { print("---") }
+   if showDetails { print("=   \(pad(string: String(result, radix: 2), toSize: 8))") }
+   if showDetails { print("-----") }
 }
 print("Second loop...")
 for number in array {
-   if showDetails { print("  \(pad(string: String(result, radix: 2), toSize: 8))") }
-   if showDetails { print("^ \(pad(string: String(number, radix: 2), toSize: 8))") }
+   if showDetails { print("    \(pad(string: String(result, radix: 2), toSize: 8))") }
+   if showDetails { print("XOR \(pad(string: String(number, radix: 2), toSize: 8))") }
    result = result ^ number
-   if showDetails { print("> \(pad(string: String(result, radix: 2), toSize: 8))") }
-   if showDetails { print("---") }
+   if showDetails { print("=   \(pad(string: String(result, radix: 2), toSize: 8))") }
+   if showDetails { print("-----") }
 }
 
-print("Missing number is \(result)")
+print("Missing number is \(result), in binary: \(pad(string: String(result, radix: 2), toSize: 8))")
 
 // Helper function for printing out details
 func pad(string : String, toSize: Int) -> String {
