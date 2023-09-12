4b5f60262b21b9f21d2591cab75d0fc2f7243dc7
Antti Juustila <antti.juustila@gmail.com>
2023-09-12 11:20:45 +0300

    Changed printouts to English so understandable also to English speaking students

--- divbyzero/divbyzero.c
+++ divbyzero/divbyzero.c
@@ -8,11 +8,11 @@ int main() {
    int proceed = 1;
 
    while (proceed) {
-      printf("Lasketaanko keskiarvoja, 0 = ei lasketa: ");
+      printf("Continue average age counting, 0 = do not continue: ");
       scanf("%d", &proceed);
       if (!proceed) break;
       int age = 0;
-      printf("Anna ika: ");
+      printf("Enter age: ");
       scanf("%d", &age);
       sumOfAges += age;
       numberOfPersons++;
@@ -23,4 +23,10 @@ int main() {
    int medianAge = sumOfAges / numberOfPersons;
    printf("Median age is: %d\n", medianAge);
    return 0;
+   // NB: compilers are developed and made less tolerant to errors like this.
+   // Below this same code was built and executed on macOS Sonoma beta with Xcode 15 beta.
+   // A floating point exception is generated to alert developers of division by zero:
+   // > ./divbyzero
+   // Continue average age counting, 0 = do not continue: 0
+   // [1]    78791 floating point exception  ./divbyzero
 }
