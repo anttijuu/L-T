c7402707796d209d9b7e4834a9cd30e1b8d8cfae
Antti Juustila <antti.juustila@gmail.com>
2024-09-16 19:22:53 +0300

    Added printing an array of bools as number, signed and unsigned

--- gates-in-c/gates.c
+++ gates-in-c/gates.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 // Include all the gates / chips
 #include "and.h"
@@ -27,6 +28,8 @@ typedef enum operationType {
 /// Print truth tables for basic operators having two inputs.
 void printTruthTable(Operation op);
 void printBooleanArrayAsBits(bool byte[]);
+void printBooleanArrayAs8BitUnsignedInt(bool byte[]);
+void printBooleanArrayAs8BitSignedInt(bool byte[]);
 
 /// main function demonstrates the logical operators and "chips" implemented
 /// using the logical operators, following the course exercise 3.
@@ -44,9 +47,24 @@ int main() {
 	// Eight bit arrays as truth values.
 	bool zeroByte[] =    {false, false, false, false, false, false, false, false}; // 00000000, 0x00, 0
 	bool nonZeroByte[] = {false, true, false,  false, false, false, false, true};  // 01000001, 0x41, 65
+	bool dec234[] = {true, true, true,  false, true, false, true, false};  // 11101010, 0xEA, 234
+
+	// Printing out some numbers
+	printf("\nPrinting out number 234...\n");
+	printf("First print the bits             : ");
+	printBooleanArrayAsBits(dec234);
+	printf("Then print the SIGNED int value  : ");
+	printBooleanArrayAs8BitSignedInt(dec234);
+	printf("\nVerify that this works ...\n");
+	int8_t verifySigned = 0b11101010; // Same bin value as above
+	printf("Should be same value as above    : %d\n", verifySigned);
+	printf("Then print the UNSIGNED int value: ");
+	printBooleanArrayAs8BitUnsignedInt(dec234);
+	uint8_t verifyUnsigned = 0b11101010; // Same bin value as above
+	printf("\nShould be same value as above    : %d", verifyUnsigned);
 
 	// Testing isZero chip.
-	printf("\nTesting isZero chip...\n");
+	printf("\n\nTesting isZero chip...\n");
 	printBooleanArrayAsBits(zeroByte);
 	printf("Is the byte above a zero integer?: %s\n\n", isZero(zeroByte) ? "Yes" : "No");
 	printBooleanArrayAsBits(nonZeroByte);
@@ -169,4 +187,22 @@ void printBooleanArrayAsBits(bool byte[]) {
 		printf("%d", byte[i] ? 1 : 0);
 	}
 	printf("\n");
+}
+
+void printBooleanArrayAs8BitUnsignedInt(bool byte[]) {
+	uint8_t value = 0;
+	for (int bitIndex = 0, bitValue = 128; bitIndex < 8; bitIndex++, bitValue = bitValue >> 1) {
+		// printf("-- value: %ud bitValue: %d\n", value, bitValue);
+		value += byte[bitIndex] ? bitValue : 0;
+	}
+	printf("%d", value);
+}
+
+void printBooleanArrayAs8BitSignedInt(bool byte[]) {
+	int8_t value = 0;
+	for (int bitIndex = 0, bitValue = 128; bitIndex < 8; bitIndex++, bitValue = bitValue >> 1) {
+		// printf("-- value: %ud bitValue: %d\n", value, bitValue);
+		value += byte[bitIndex] ? bitValue : 0;
+	}
+	printf("%d", value);
 }
